2c792c8a0fb3e665ffd0b07f709fbf89e5b60b37
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2023-04-23 18:29:30 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -6,11 +10,33 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('PoseNet is Intitialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftWristX +"leftWristY = "+ leftWristY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY)
+    }
+    
 }
 
 function draw() {
     image(video, 0, 0, 600, 500)
 }
+
 
 function preload()
 {
@@ -19,4 +45,7 @@ function preload()
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
